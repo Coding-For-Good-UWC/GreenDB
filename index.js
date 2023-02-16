fda3e0b1ec584d2172cd2ee93c423ecdbde7325b
Antoine-Lee <antoinekllee@gmail.com>
2023-02-16 15:45:34 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config() // stores all key value pairs in dotenv in process.env
+
+const express = require ('express'); 
+const mongoose = require ('mongoose'); 
+
+const { PORT=3000, MONGO_URI } = process.env; // provide default value in case if not available in .env file
+
+const app = express (); 
+
+app.use (express.json()); 
+app.use (express.urlencoded({ extended: true })); // replace certain invalid characters for the url // %20 -> space
+
+mongoose.set('strictQuery', false);
+mongoose.connect (MONGO_URI, () => 
+{
+    console.log ("CONNECTED TO DB"); 
+}); // connect to mongodb instance
+
+app.listen (PORT, () => // fire up express server
+{
+    console.log ("LISTENING ON PORT " + PORT); 
+})
